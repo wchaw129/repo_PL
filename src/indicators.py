5872134662d245e93070c1223d5faef13fbabcfe
wchaw03 <w-chawar@wp.pl>
2025-12-20 22:28:50 +0100

    stockwatch.pl scraping

--- src/indicators.py
+++ src/indicators.py
@@ -1,12 +1,29 @@
-def ROA(financials, balance_sheet):
-    return financials.loc['NetIncome'] / balance_sheet.loc['TotalAssets']
+import pandas as pd
+import requests
+from io import StringIO
+import re
 
-def CurrentRatio(balance_sheet):
-    return balance_sheet.loc['CurrentAssets'] / balance_sheet.loc['CurrentLiabilities']
+def GetData(company_name: str) -> pd.DataFrame | None:
+    url_mainpage = f'https://www.stockwatch.pl/gpw/{company_name},notowania,dane-finansowe.aspx'
+    header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'} 
+    header_new = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+        'Referer': url_mainpage,  
+        'X-Requested-With': 'XMLHttpRequest' 
+    }
+    try:
+        r = requests.get(url_mainpage, headers=header)
+        r.raise_for_status()
+        
+        cid = re.search(r"var\s+companyId\s*=\s*'(\d+)';", r.text).group(1)
+        isbank = re.search(r"var\s+isBank\s*=\s*'(\w+)';", r.text).group(1)
 
-def DebtRatio(balance_sheet):
-    return balance_sheet.loc['TotalLiabilitiesNetMinorityInterest'] / balance_sheet.loc['TotalAssets']
+        url_api = f'https://www.stockwatch.pl/async/FinancialDataAsync.aspx?companyId={cid}&period=y&isBank={isbank}'
+        r_new = requests.get(url_api, headers = header_new)
+        df = pd.read_html(StringIO(r_new.text))[0]
+        print(df)
+    except Exception as e:
+        print(f"Wystąpił błąd: {e}")
 
-def GrowthPotential(cash_flow, balance_sheet):
-    # CAPEX / aktywa
-    return cash_flow.loc['Capital Expenditure'] / balance_sheet.loc['TotalAssets']
+
+
