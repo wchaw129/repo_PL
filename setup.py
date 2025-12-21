3a2d91ed26d883c308e8a3034168d2b5210aeca0
wchaw03 <w-chawar@wp.pl>
2025-12-21 14:53:33 +0100

    Directiories implementation

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kmeans-gpw',
+    version='0.1',
+    packages=find_packages(),
+)
---
+++ src/config.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+SRC_DIR = Path(__file__).parent
+ROOT_DIR = SRC_DIR.parent
+DATA_DIR = ROOT_DIR / 'data'
